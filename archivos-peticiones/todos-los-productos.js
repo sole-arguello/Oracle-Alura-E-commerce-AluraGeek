47062f74055bc04324a35d5fad645b1da4cf5ceb
Soledad <soledad.ar1@mail.com>
2022-05-13 19:23:32 -0300

    ..

--- archivos-peticiones/todos-los-productos.js
+++ archivos-peticiones/todos-los-productos.js
@@ -1,11 +1,30 @@
-import { base_de_datos } from "./base-datos-json.js";
 
+const rutaImagenes = "./imagenes/galeria-productos/";
+function mostrartodosLosProductos(todosLosProductos){
 
-// direccciono los links de ver todos
-const urlVertodos= window.location.search;
+    const todos = document.getElementById("todos_los_productos");
 
-const vertodos = new URLSearchParams(urlVertodos);
+    todosLosProductos.forEach(categ =>{
+        categ.productos.forEach(prod =>{
+            const listaTodos= document.createElement("li");
+            listaTodos.classList.add("productos__card");
+            listaTodos.innerHTML = `
+            
+                <img class="productos__img" src="${rutaImagenes}${prod.imagen}"> 
+                <p class="producto__descripcion">${prod.nombre}</p>
+                <p class="producto__precio">${prod.precio}</p>
+                <a class="producto__link" href="./productos.html?id_producto=${prod.id}">Ver producto</a>
+            
+            
+            `;
+            todos.appendChild(listaTodos);
+        });      
+	});
+    
+}
 
-const nombre_categoria = vertodos.get("nombre_categoria");
-
-console.log(nombre_categoria);
+const urlTodosProd= `http://localhost:3000/productos`;
+fetch(urlTodosProd)
+.then( res => { return res.json() } )
+.then( data => {mostrartodosLosProductos(data); })
+.catch (err => { console.log(err)});

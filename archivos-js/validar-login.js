82531f043ff54e96dc6545f8aa0983d203a32322
Soledad <soledad.ar1@mail.com>
2022-06-02 10:50:01 -0300

    se corrije una falla en el login

--- archivos-js/validar-login.js
+++ archivos-js/validar-login.js
@@ -1,3 +1,10 @@
+
+//capturo el boton enviar y le envio la funcion 
+const btnEnviar = document.querySelector('#btnEnviarLogin');
+btnEnviar.addEventListener('click', (e) => {
+    e.preventDefault();
+    loginUsuario();
+});
 //validacion de ususario y password
 const loginUsuario = () =>{
 
@@ -17,7 +24,7 @@ const loginUsuario = () =>{
             title: 'Login Exitoso',
             showConfirmButton: false,
             timer: 1500
-        });
+        })
       
         setTimeout(function(){
             window.location.href = "../screens/todos-productos-editables.html";
@@ -33,10 +40,3 @@ const loginUsuario = () =>{
     correoIgresado.reset();
     contraseniaIngresada.reset();
 }
-
-//capturo el boton enviar y le envio la funcion 
-const btnEnviar = document.querySelector('#btnEnviarLogin');
-btnEnviar.addEventListener('click', (e) => {
-    e.preventDefault();
-    loginUsuario();
-});
